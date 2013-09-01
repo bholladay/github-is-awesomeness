3b9c7a50aa46a8db008f0d0624a789ef679f90cc
Alan Orth <alan.orth@gmail.com>
2013-09-01 20:21:19 +0300

    fabfile.py: Add prepare() to copy reveal.js assets
    Since we're using reveal.js as a submodule, we want want to have
    the latest and greatest js, css, etc...

--- fabfile.py
+++ fabfile.py
@@ -4,8 +4,15 @@ from jinja2 import Environment, FileSystemLoader
 
 template_env = Environment(loader=FileSystemLoader('.'))
 
+# copy reveal.js assets from submodule to presentation
+def prepare():
+    local('cp -r reveal.js/css/* presentation/css/')
+    local('cp -r reveal.js/js/* presentation/js/')
+    local('cp -r reveal.js/lib/* presentation/lib/')
+    local('cp -r reveal.js/plugin/* presentation/plugin/')
 
 def build():
+    prepare()
     template = template_env.get_template('source/index.html')
     rendered_template = template.render()
     with open('presentation/index.html', 'wb') as fh:
