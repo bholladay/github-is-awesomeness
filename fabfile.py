383db331ff437fd2fc3e10b47e6d320110098f99
Alan Orth <alan.orth@gmail.com>
2013-12-10 13:35:24 +0300

    fabfile.py: Clean up and bulletproof
    Make sure to create necessary presentation directories so we can
    copy the presentation output there.

--- fabfile.py
+++ fabfile.py
@@ -6,10 +6,10 @@ template_env = Environment(loader=FileSystemLoader('.'))
 
 # copy reveal.js assets from submodule to presentation
 def prepare():
-    local('cp -r reveal.js/css/* presentation/css/')
-    local('cp -r reveal.js/js/* presentation/js/')
-    local('cp -r reveal.js/lib/* presentation/lib/')
-    local('cp -r reveal.js/plugin/* presentation/plugin/')
+    local('mkdir -p presentation/css && cp -r reveal.js/css/* presentation/css/')
+    local('mkdir -p presentation/js && cp -r reveal.js/js/* presentation/js/')
+    local('mkdir -p presentation/lib && cp -r reveal.js/lib/* presentation/lib/')
+    local('mkdir -p presentation/plugin && cp -r reveal.js/plugin/* presentation/plugin/')
 
 def build():
     prepare()
