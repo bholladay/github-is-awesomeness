a094315269cfbf5cc3334c08744a8baab594f991
Bryce Holladay <bhsacto@gmail.com>
2015-10-17 16:22:48 -0700

    Add images and update fabfile to support them.

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,20 @@ from jinja2 import Environment, FileSystemLoader
 
 template_env = Environment(loader=FileSystemLoader('.'))
 
+
 # copy reveal.js assets from submodule to presentation
 def prepare():
-    local('mkdir -p presentation/css && cp -r reveal.js/css/* presentation/css/')
-    local('mkdir -p presentation/js && cp -r reveal.js/js/* presentation/js/')
-    local('mkdir -p presentation/lib && cp -r reveal.js/lib/* presentation/lib/')
-    local('mkdir -p presentation/plugin && cp -r reveal.js/plugin/* presentation/plugin/')
+    local('mkdir -p presentation/css && \
+          cp -r reveal.js/css/* presentation/css/')
+    local('mkdir -p presentation/js && \
+          cp -r reveal.js/js/* presentation/js/')
+    local('mkdir -p presentation/lib && \
+          cp -r reveal.js/lib/* presentation/lib/')
+    local('mkdir -p presentation/plugin && \
+          cp -r reveal.js/plugin/* presentation/plugin/')
+    local('mkdir -p presentation/images && \
+          cp -r source/images/* presentation/images/')
+
 
 def build():
     prepare()
@@ -17,6 +25,7 @@ def build():
     rendered_template = template.render()
     with open('presentation/index.html', 'wb') as fh:
         fh.write(rendered_template)
+
 
 def publish():
     local('ghp-import -p presentation')
